a78938d0b25ecd4967d0f57569fbb2d660656b0b
Cynthia-Melo <cynthia.00000860095@unicap.br>
2025-08-16 00:10:41 +0000

    Exemplo com for

---
+++ Exemplo01.java
@@ -0,0 +1,7 @@
+public class Exemplo01 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            System.out.printf("%02d Olá turma!%n", i);
+        }
+    }
+}
