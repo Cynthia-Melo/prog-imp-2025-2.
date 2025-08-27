3ee818a418de516ab64b7c7cb04f43919ed2553f
Cynthia Melo <cynthia.00000860095@unicap.br>
2025-08-27 06:01:09 -0300

    Update ExercicioMenu.java

--- ExercicioMenu.java
+++ ExercicioMenu.java
@@ -6,10 +6,38 @@ public class ExercicioMenu{
         do {
             System.out.println("1- Imprimir todos os numeros entre 1 e 100");
             System.out.println("2- Imprimir todos os numemeros pares entre 1 e 100");
-            System.out.println("3- Impromir todos os numeros impara");
-            
-        }
+            System.out.println("3- Impromir todos os numeros impares entre 1 e 100");
+            System.out.println( "4- Sair");
 
+            System.out.println( "Digite a sua opção");
+            opcao = input.nextInt();
+
+            switch (opcao){
+                case 1:
+                for (int c = 1; c <= 100; c++){
+                    break;
+
+                    case 2:
+                    for ( int c = 2; c <= 100; c += 2 ){
+                    break;
+
+                    case 3:
+                    for ( int c = 1; c <= 100; c += 2);
+                    break;
+
+                    case 4:
+                    System.out.println("Saindo");
+                    break;
+
+
+                }
+
+
+            }
+            
+        } 
+        
     }
+}
   
   
