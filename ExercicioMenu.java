bee5394bdb2c32be360f9adf252ac18c22f19fa5
Cynthia Melo <cynthia.00000860095@unicap.br>
2025-08-26 23:29:51 -0300

    Update ExercicioMenu.java

--- ExercicioMenu.java
+++ ExercicioMenu.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
+public class ExercicioMenu{
+    public static Scanner input = new Scanner( System.in);
+    public static void main(String []argas){
+        int opcao;
+        do {
+            System.out.println("1- Imprimir todos os numeros entre 1 e 100");
+            System.out.println("2- Imprimir todos os numemeros pares entre 1 e 100");
+            System.out.println("3- Impromir todos os numeros impara");
+            
+        }
 
+    }
+  
+  
